298ee63cbf9f415c0683dfd72a13cedc011e0335
Stoeltie <ferdistoeltie@gmail.com>
2021-07-20 15:59:00 +0200

    Added initial javascript example lesson01

--- server.js
+++ server.js
@@ -1,3 +1,29 @@
 /**
- * 
+ * This lesson will introduce you in the world of NodeJS and Javascript.
+ * It will contain assignments that have to be run succesfully.
+ * Follow the readme of this lesson to complete the lesson.
  */
+
+/**
+ * Making sure your environment works:
+ * One of the most used functions in Javascript is the console.log() function. This function prints a message written between the '(' and ')' to the output terminal/console.
+ * Note the following: a function will always follow this format: 'functionname()'. 
+ * The assignment below is to make you familiar with using the most common javascript function: console.log and variables.
+ */
+
+// This should print the text: 'Lesson01' to the terminal screen.
+console.log("Lesson01")
+
+// Feel free to change the text here to something else and see the result.
+console.log("change text")
+
+// We can also print numbers to the console:
+console.log(10)
+
+// We can also print the value that a variable holds:
+const variable1 = "text variable1"
+console.log(variable1)
+
+// Feel free to chagne the value here:
+const variable2 = "change the text of variable2"
+console.log(variable2)
